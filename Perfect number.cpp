6c119808ed9dbbed486734a39189b0ce6856242a
Anirudh Belwadi <42411311+anirudhbelwadi@users.noreply.github.com>
2020-10-11 15:28:39 +0400

    Update Perfect number.cpp

--- Perfect number.cpp
+++ Perfect number.cpp
@@ -1,29 +1,23 @@
 #include<iostream>
 using namespace std;
-int main()
-{
+int main(){
 	int num,numcpy=0,j=0;
 	cout<<"Enter the number : ";
 	cin>>num;
 	int factor[num];	//this is an array
-	for(int i=1;i<=num-1;i++)
-	{
-		if(num%i==0)
-		{
+	for(int i=1;i<=num-1;i++){
+		if(num%i==0){
 			factor[j]=i;
 			j++;
 		}
 	}
-	for(int o=0;o<j;o++)
-	{
+	for(int o=0;o<j;o++){
 		numcpy=numcpy+factor[o];
 	}
-	if(numcpy==num)
-	{
+	if(numcpy==num){
 		cout<<"\nNumber is perfect number.";
 	}
-	else
-	{
+	else{
 		cout<<"\nNumber is not perfect number.";
 	}
 	return 0;
